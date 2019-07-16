a29ea9912df34bc1d654f4bd3224b46832d7a0dd
Carlos Gabriel <carlosggflor@gmail.com>
2019-07-16 01:34:10 -0300

    Implemented To Do List

--- meuApp/src/app/home/home.page.ts
+++ meuApp/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public list: Array<any> = [
+    { name: 'First Element', id: 1 },
+    { name: 'Second Element', id: 2 }
+  ];
 
+  @ViewChild('inp') elementRef: any;
+
+  constructor() { }
+
+  add() {
+    if (this.list.length > 0) {
+      this.list.push({ name: this.elementRef.el.value, id: this.list[this.list.length - 1].id + 1 });
+    } else {
+      this.list.push({ name: this.elementRef.el.value, id: 0 });
+    }
+    this.clean();
+  }
+
+  remove(_item) {
+    const indexToExclude = this.list.findIndex(item => item.id === _item.id);
+    this.list.splice(indexToExclude, 1);
+  }
+
+  edit(_item) {
+    if (this.elementRef.el.value === undefined) { return };
+    if (this.elementRef.el.value.trim() === '') { return };
+    _item.name = this.elementRef.el.value;
+    this.clean();
+  }
+
+  clean() {
+    this.elementRef.el.value = "";
+  }
 }
