614ff86647b978894151093a60e9496c932b7e9e
Carlos Gabriel <carlosggflor@gmail.com>
2019-07-18 01:15:04 -0300

    Advanced To Do List

--- meuApp/src/app/home/home.page.ts
+++ meuApp/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,37 +8,80 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 })
 export class HomePage {
 
-  public list: Array<any> = [
-    { name: 'First Element', id: 1 },
-    { name: 'Second Element', id: 2 }
+  public persons: Array<Person> = [
+    {
+      id: 1,
+      name: 'Carlos',
+      surname: 'Flor',
+      age: 22
+    },
+    {
+      id: 2,
+      name: 'Lourenco',
+      surname: 'Milhati',
+      age: 52
+    }
   ];
 
-  @ViewChild('inp') elementRef: any;
+  @ViewChild('f') form: NgForm;
 
   constructor() { }
 
-  add() {
-    if (this.list.length > 0) {
-      this.list.push({ name: this.elementRef.el.value, id: this.list[this.list.length - 1].id + 1 });
+  // tslint:disable-next-line: use-life-cycle-interface
+  ngAfterViewInit(): void {
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+    this.clean();
+  }
+
+  onSubmit(form: NgForm) {
+    const person: Person = this.deepCopy(form.value) as Person;
+    if (this.persons.length > 0) {
+      person.id = this.persons[this.persons.length - 1].id + 1;
     } else {
-      this.list.push({ name: this.elementRef.el.value, id: 0 });
+      person.id = 0;
     }
-    this.clean();
+
+    this.persons = [...this.persons, person];
   }
 
-  remove(_item) {
-    const indexToExclude = this.list.findIndex(item => item.id === _item.id);
-    this.list.splice(indexToExclude, 1);
+  // tslint:disable-next-line: variable-name
+  public remove(_person: Person) {
+    const indexToExclude = this.findIndex(_person);
+    this.persons.splice(indexToExclude, 1);
   }
 
-  edit(_item) {
-    if (this.elementRef.el.value === undefined) { return };
-    if (this.elementRef.el.value.trim() === '') { return };
-    _item.name = this.elementRef.el.value;
-    this.clean();
+  // tslint:disable-next-line: variable-name
+  public edit(_person: Person) {
+    const form: NgForm = this.form;
+    if (!form.valid) { return; }
+    const { name, surname, age } = this.deepCopy(form.value) as Person;
+    _person.name = name;
+    _person.surname = surname;
+    _person.age = age;
   }
 
-  clean() {
-    this.elementRef.el.value = "";
+  private clean() {
+    this.form.ngSubmit.subscribe(form => {
+      form.srcElement.reset();
+    });
   }
+
+  // tslint:disable-next-line: variable-name
+  private findIndex(_person: Person) {
+    const index = this.persons.findIndex(person => person.id === _person.id);
+    return index;
+  }
+
+  // tslint:disable-next-line: ban-types
+  private deepCopy(obj: Object): Object {
+    return JSON.parse(JSON.stringify(obj));
+  }
+}
+
+interface Person {
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
 }
